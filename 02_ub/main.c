f5e2aebf05b5b751f94916cedf327b31ee8a7791
oe-bayram <oe-bayram@live.de>
2017-05-26 19:56:51 +0200

    added changes

--- 02_ub/main.c
+++ 02_ub/main.c
@@ -432,10 +432,19 @@ int main(int argc, char **argv)
       local_address[2] = B.rows;
       local_address[3] = B.columns;
       pos +=4;
-      memcpy(pos, A.matrix, A_size * sizeof(int));
-      pos +=A_size;
-      memcpy(pos, B.matrix, B_size * sizeof(int));
 	  int counter;
+	  for(counter = 0; counter<120;counter++){
+		 printf("%d. Value of pos[%d]: %d\n", counter+1, counter, pos[counter]); 
+	  }
+      memcpy(pos, A.matrix, A_size * sizeof(int));
+	  for(counter = 0; counter<120;counter++){
+		 printf("%d. Value of pos[%d]: %d\n", counter+1, counter, pos[counter]); 
+	  }
+      pos +=A_size;
+	  for(counter = 0; counter<120;counter++){
+		 printf("%d. Value of pos[%d]: %d\n", counter+1, counter, pos[counter]); 
+	  }
+      memcpy(pos, B.matrix, B_size * sizeof(int));
 	  for(counter = 0; counter<120;counter++){
 		 printf("%d. Value of pos[%d]: %d\n", counter+1, counter, pos[counter]); 
 	  }
