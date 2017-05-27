3ae7cf10eec1f5d32c0b135cbb05f36a4cb7cb6c
oe-bayram <oe-bayram@live.de>
2017-05-27 16:38:43 +0200

    added changes

--- 02_ub/main.c
+++ 02_ub/main.c
@@ -472,10 +472,6 @@ int main(int argc, char **argv)
 	  C_pos += 4 + A_size + B_size;
 	  C_pos += (comm_size-1) * chunk_size * B.columns;
 	  memcpy(C_pos, C_part.matrix, C_part.rows * C_part.columns * sizeof(int));
-
-	  for(counter = 0; counter<120;counter++){
-		 printf("%d. Value of pos[%d]: %d\n", counter+1, counter, pos[counter]); 
-	  }
 	  
       MPI_Barrier(MPI_COMM_WORLD);
 	  
