cad46b5f5dac506db56ffb86e8872854641d8cb7
oe-bayram <oe-bayram@live.de>
2017-06-18 01:46:49 +0200

    added changes

--- 04_ub/nbodydsm.c
+++ 04_ub/nbodydsm.c
@@ -258,8 +258,9 @@ void work(int node_id, int comm_size, point *points, int full_size, int iteratio
     int i;
     for(i = 0; i < iteration; i++)
     {
-        //printf("%d: Started computing for iteration: %d and points are: \n", node_id, i);
-        //print_points_segment(segment, node_id, i);
+        printf("%d: Started computing for iteration: %d and points are: \n", node_id, i);
+        print_points_segment(segment, node_id, i);
+        print_points(points, full_size, node_id, i);
         compute_movement(points, point_vel, offset, compute_size, full_size, segment, node_id, i);
         read_points_segment(segment, &points, &full_size, node_id);
         //update_points(comm_size, points, full_size);
